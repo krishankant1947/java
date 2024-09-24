6a1947a71fd8ee6f9444b0af2c4be4a9781214a1
Krishna Kant <kantk2492@gmail.com>
2024-09-24 10:46:19 +0530

    php

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php
+
+// error_reporting(0);
+// ini_set('display_errors', 0);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<?php
+$age = 1;;
+$blacklisted = true;
+
+var_dump($age>18 && !$blacklisted);
+
+if($age > 18 && !$blacklisted){
+    echo "You are 18+ and can vote";
+} elseif($age>18 && $blacklisted) {
+    echo "You cannot vote becusey you are banned";
+} else{
+    echo "You cannot     vote";
+}
+
+echo 1/1;
+
+$name = "viapn Kumar";
+echo $name;
+
+?>
+<body>
+    <table border=1 width=100%>
+    <?php
+
+echo "asdf a","sadfasdfas","eeeeeeeeeeeee";
+print("asdf a");
+
+print("asdf a");
+print("<br/>*<br/>");
+print("**<br/>");
+for($a=1;  $a<=10; $a = $a+1){
+    // echo <<<'KR'
+    //     <tr>
+    //         <td>" '{$a}</td>
+    //         <td>2</td>
+    //         <td>sdf</td>
+    //         <td>asdf</td>
+    //     </tr>
+    //     KR;
+    echo  "<tr>
+            <td>{$a}</td>
+            <td>2</td>
+            <td>sdf</td>
+            <td>asdf</td>
+        </tr>";
+}
+    ?>
+    </table>
+    <table border="2" width="100%">
+        <tr>
+            <td>asdasdf</td>
+            <td>z</td>
+            <td>sadf</td>
+            <td>xadf</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</body>
+</html>
