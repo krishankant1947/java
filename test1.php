d8d547fe59df5155eccbf313fbe432de0b377c34
Krishna Kant <kantk2492@gmail.com>
2024-10-04 10:57:34 +0530

    jh

--- test1.php
+++ test1.php
@@ -7,8 +7,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script type="text/javascript">
-        function element(){
-            
+        function checkpassword(){
+            //alert("abcd");
+            console.log("userpasswordentry");
             if(userpasswordentry.length != 0){
                     if(userpasswordentry== userreentry){
                         message.textContent ="password match";
@@ -18,6 +19,12 @@
                     }
                 }
         }
+         function trackevents(event){
+            event.preventDefault();
+            pass = document.getElementById("Userpassword").value;
+            alert("dsa")
+           // console.log(event.type)
+         }
     </script>
 </head>
 <body>
@@ -31,9 +38,9 @@
 
 
                         <?php
-                if( empty($_GET['userpasswordentry']) ){
-                    echo '<div class="alert alert-danger">password are not match</div>';
-                }
+              //  if( empty($_get['userpasswordentry']) ){
+                //    echo '<div class="alert alert-danger">password are not match</div>';
+               // }
 
 
                 ?>
@@ -43,7 +50,7 @@
                     <div class="col-12">
                         <div class="form-floating">
                             <input type="text" class="form-control" name="userentry"
-                            value= "<?php  echo isset($_GET["userentry"])  ?$_GET['userentry']:"control";?>" 
+                            value= "<?php  echo isset($_get["userentry"]) ?$_get['userentry']:"";?>" 
                             id="Usernamelogin">
                             <label for="Usernamelogin">Username:</label>
                         </div>
@@ -53,7 +60,7 @@
                     <div class="col-12">
                         <div class="form-floating">
                             <input type="text"  class="form-control" name="useremailentry"
-                            value= "<?php  echo isset($_GET["useremailentry"])  ?$_GET['useremailentry']:"blank";?>" 
+                            value= "<?php  echo isset($_get["useremailentry"])  ?$_get['useremailentry']:"";?>" 
                             id="Useremaillogin">
                             <label for="Useremaillogin">Email:</label>
                         </div>
@@ -78,7 +85,10 @@
                 <div class="row">
                 <div class="col-6">
                     <div class="form-floating" >
-                <button type="submit" class="btn btn-danger" >login </button>
+                <button type="submit" class="btn btn-danger" onblur="checkpassword(event,this)" 
+                onclick="trackevents(event)"
+
+                >login </button>
                 </div>
                 </div>
             </div>
@@ -86,23 +96,51 @@
         </div><!--row-->
 
         <?php
-    if(empty($_GET["userentry"])){     
+    if(empty($_get["userentry"])){     
         echo "enter user name";
     }
     else {
-        printf("correct username %s ?", $_GET['userentry']);
+        printf("correct username %s ?", $_get['userentry']);
     }
-  //  print_r($_GET);
+  //  print_r($_get);
 
 
-if(empty($_GET["useremailentry"])){
+if(empty($_get["useremailentry"])){
     echo "sorry your email are not found";
 }
 else {
-    printf("Are you sure your email %s ", htmlspecialchars_decode($_GET['useremailentry']));
+    printf("Are you sure your email %s ",($_get['useremailentry']));
 }
-//print_r($_GET);
+
     ?>
     </form>
 </body>
 </html>
+userpasswordentry
+test1.php?userentry=&useremailentry=&userpasswordentry=tr&userreentry=:13 Uncaught ReferenceError: userpasswordentry is not defined
+    at checkpassword (test1.php?userentry=&useremailentry=&userpasswordentry=tr&userreentry=:13:13)
+    at HTMLButtonElement.onblur (test1.php?userentry=&useremailentry=&userpasswordentry=tr&userreentry=:84:18)
+checkpassword @ test1.php?userentry=&useremailentry=&userpasswordentry=tr&userreentry=:13
+onblur @ test1.php?userentry=&useremailentry=&userpasswordentry=tr&userreentry=:84Understand this error
+pass = document.getElementById('Userpassword').value
+''
+pass
+''
+test1.php?userentry=&useremailentry=&userpasswordentry=tr&userreentry=:12 userpasswordentry
+test1.php?userentry=&useremailentry=&userpasswordentry=tr&userreentry=:13 Uncaught ReferenceError: userpasswordentry is not defined
+    at checkpassword (test1.php?userentry=&useremailentry=&userpasswordentry=tr&userreentry=:13:13)
+    at HTMLButtonElement.onblur (test1.php?userentry=&useremailentry=&userpasswordentry=tr&userreentry=:84:18)
+checkpassword @ test1.php?userentry=&useremailentry=&userpasswordentry=tr&userreentry=:13
+onblur @ test1.php?userentry=&useremailentry=&userpasswordentry=tr&userreentry=:84Understand this error
+pass = document.getElementById('Userpassword').value
+'ssssssssssssssssssssss'
+pass
+'ssssssssssssssssssssss'
+pass = document.getElementById('Userpassword').value
+console.log(pass)
+VM1728:2 ssssssssssssssssssssss
+undefined
+confirm = document.getElementById('Userconfirmpass').value
+'asdfasdfasdf'
+pass == confirm
+false
