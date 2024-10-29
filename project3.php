f2f92c81c33ec05cbb79410e141b7c97ecf8c94d
krishankant1947 <kantk2492@gmail.com>
2024-10-29 08:42:30 +0530

    hello

---
+++ project3.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <form action="">
+        <div class="card">
+            <div class="card-body">
+                <h2>Academic Details</h2>
+                <div class="row my-3">
+                    <div class="col-2"></div>
+                    <div class="col-2">
+                        <h6>BOARD</h6>
+                    </div>
+                    <div class="col-2">
+                        <h6>YEAR OF PASSING</h6>
+                    </div>
+                    <div class="col-3">
+                        <h6>STATE FROM WHERE COMPLETED</h6>
+                    </div>
+                    <div class="col-3">
+                        <h6>RESULT
+                            (CGPA/PERCENTAGE)</h6>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-2">
+                        <h5>X STD</h5>
+                    </div>
+                    <div class="col-2">
+                         
+                        <input type="text" value="hello" class="form-control">
+                       <label for="" > X STD </label>
+                    </div>
+                    <div class="col-2"></div>
+                    <div class="col-3"></div>
+                    <div class="col-3"></div>
+                </div>
+                <div class="row">
+                    <div class="col-2"></div>
+                    <div class="col-2"></div>
+                    <div class="col-2"></div>
+                    <div class="col-3"></div>
+                    <div class="col-3"></div>
+                </div>
+                <div class="row">
+                    <div class="col-2"></div>
+                    <div class="col-2"></div>
+                    <div class="col-2"></div>
+                    <div class="col-3"></div>
+                    <div class="col-3"></div>
+                </div>
+            </div><!--card-body-->
+        </div>
+    </form>
+</body>
+</html>
