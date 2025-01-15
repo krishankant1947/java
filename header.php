433516b3c6f102c16fb782ade35bf38b216a7ea2
krishankant1947 <kantk2492@gmail.com>
2025-01-15 10:17:53 +0530

    n

---
+++ header.php
@@ -0,0 +1,30 @@
+<nav class=" main-header navbar navbar-expand">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+        <a href="#"  class="nav-item m-1 p-1">#</a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="#"  class="nav-item m-1 p-1">Home</a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+        <a href="#"  class="nav-item m-1 p-1">contact</a>
+        </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a href="#" class="nav-item m-1 p-1">a</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-item m-1 p-1">c</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-item m-1 p-1">v</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-item m-1 p-1">b</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-item m-1 p-1">n</a>
+        </li>
+    </ul>
+</nav>
---
+++ sidebar.php
@@ -0,0 +1,55 @@
+<aside class="main-sidebar sidebar-light-info border ">
+        <span>AdminLTE 3</span>
+        <div class="sidebar">
+            <div>
+                Krishan kant
+            </div>
+            <div>fad</div>
+            <nav class="mt-2">
+                <ul>
+                    <li class="nav-item">hello
+                      <a href="#" class="nav-link"></a>
+                      <ul class="nav nav-treeview" style="display:block;">
+                            <li class="nav-item menu-is-opening menu-open">
+                                <a href="" class="nav-link">
+                                  <i class="nav-icon far fa-plus-square"></i>
+                                  <p>Invoice
+                                    <i class="far fa-angle-left right"></i>
+                                  </p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="">
+                                  <i></i>
+                                  <p>Invoice</p>
+                                </a></li>
+                            <li class="nav-item">
+                                <a href="">
+                                  <i></i>
+                                  <p>Invoice</p>
+                                </a></li>
+                            <li class="nav-item">
+                                <a href="">
+                                  <i></i>
+                                  <p>Invoice</p>
+                                </a></li>
+                            <li class="nav-item">
+                                <a href="">
+                                  <i></i>
+                                  <p>Invoice</p>
+                                </a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">SF</li>
+                    <li class="nav-item">SF</li>
+                    <li class="nav-item">SF</li>
+                    <li class="nav-item">SF</li>
+                    <li class="nav-item">SF</li>
+                    <li class="nav-item">SF</li>
+                    <li class="nav-item">SF</li>
+                    <li class="nav-item">SF</li>
+                    <li class="nav-item">SF</li>
+                </ul>
+            </nav>
+        </div><!--sidebar-->
+</aside>
